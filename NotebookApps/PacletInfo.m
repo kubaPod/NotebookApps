df64a49f75f84ed2e230f4928d81ce4277ce416d
kuba <kuba@gluonvision.com>
2017-12-15 08:31:41 +0100

    fix(dev) PacletInfo.m init

---
+++ NotebookApps/PacletInfo.m
@@ -0,0 +1,4 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+Paclet[Name -> "NotebookApps", Version -> "0.0.1", MathematicaVersion -> "10+",
+  Description -> "Mini framework for single notebook apps creation.", Creator -> "Jakub (Kuba) Podkalicki",
+  Extensions -> {{"Kernel", Context -> "NotebookApps`"}}]
