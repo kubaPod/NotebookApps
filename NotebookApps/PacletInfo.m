9c35ec7603fa12cbfa9d7a2f0ec8526668cd2328
Kuba Podkalicki <kuba.pod@gmail.com>
2018-02-13 23:30:26 +0100

    fix() aligned initialization screen

--- NotebookApps/PacletInfo.m
+++ NotebookApps/PacletInfo.m
@@ -1,4 +1,4 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Name -> "NotebookApps", Version -> "0.2.0", MathematicaVersion -> "10+",
+Paclet[Name -> "NotebookApps", Version -> "0.3.1", MathematicaVersion -> "10+",
   Description -> "Mini framework for single notebook apps creation.", Creator -> "Jakub (Kuba) Podkalicki",
   Extensions -> {{"Kernel", Context -> "NotebookApps`"}}]
