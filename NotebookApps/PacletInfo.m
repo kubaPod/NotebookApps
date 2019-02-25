e21625005e2a51e1074a4fb9ae84fb1f71a1d6bd
Kuba <kuba.pod@gmail.com>
2019-02-25 10:51:28 +0100

    0.7.0

--- NotebookApps/PacletInfo.m
+++ NotebookApps/PacletInfo.m
@@ -1,5 +1,5 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Name -> "NotebookApps", Version -> "0.6.0", MathematicaVersion -> "10+", 
+Paclet[Name -> "NotebookApps", Version -> "0.7.0", MathematicaVersion -> "10+", 
  Description -> "Mini framework for single notebook apps creation.", 
  Creator -> "Jakub (Kuba) Podkalicki", 
  Extensions -> {{"Kernel", Context -> "NotebookApps`"}}]
